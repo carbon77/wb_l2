338e588da3af3f25825b1f955f2550b4ca7dfdb8
Igor <zakatov0303@gmail.com>
2024-12-10 13:20:48 +0300

    Add l2.2 task

---
+++ l2_2.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+func main() {
+	timeResult, err := ntp.Time("time.google.com")
+	if err != nil {
+		log.Fatalf("Ошибка в получении времени: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Текущее время: %v\n", timeResult.Format(time.UnixDate))
+}
