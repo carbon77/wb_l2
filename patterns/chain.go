f107a6c31769f6e8bac8a843df8c5c2e63828e34
Igor <zakatov0303@gmail.com>
2024-12-15 11:53:24 +0300

    Add l2.1 task: chain of responsibility

---
+++ patterns/chain.go
@@ -0,0 +1,80 @@
+package main
+
+import "fmt"
+
+type Handler interface {
+	SetNext(handler Handler) Handler
+	Handle(request *SupportRequest) string
+}
+
+type BaseHandler struct {
+	nextHandler Handler
+}
+
+func (bh *BaseHandler) SetNext(handler Handler) Handler {
+	bh.nextHandler = handler
+	return handler
+}
+
+func (bh *BaseHandler) Handle(request *SupportRequest) string {
+	if bh.nextHandler != nil {
+		return bh.nextHandler.Handle(request)
+	}
+	return "Никто не может обработать данный запрос"
+}
+
+type JuniorSupportHandler struct {
+	BaseHandler
+}
+
+func (jh *JuniorSupportHandler) Handle(request *SupportRequest) string {
+	if request.Level == 1 {
+		return fmt.Sprintf("Младший сотрудник обработал запрос: %s", request.Issue)
+	}
+	return jh.BaseHandler.Handle(request)
+}
+
+type SeniorSupportHandler struct {
+	BaseHandler
+}
+
+func (sh *SeniorSupportHandler) Handle(request *SupportRequest) string {
+	if request.Level == 2 {
+		return fmt.Sprintf("Старший сотрудник обработал запрос: %s", request.Issue)
+	}
+	return sh.BaseHandler.Handle(request)
+}
+
+type ManagerHandler struct {
+	BaseHandler
+}
+
+func (mh *ManagerHandler) Handle(request *SupportRequest) string {
+	if request.Level == 3 {
+		return fmt.Sprintf("Менеджер обработал запрос: %s", request.Issue)
+	}
+	return mh.BaseHandler.Handle(request)
+}
+
+type SupportRequest struct {
+	Level int
+	Issue string
+}
+
+func main() {
+	junior := &JuniorSupportHandler{}
+	senior := &SeniorSupportHandler{}
+	manager := &ManagerHandler{}
+
+	junior.SetNext(senior).SetNext(manager)
+
+	requests := []SupportRequest{
+		{Issue: "low level issue", Level: 1},
+		{Issue: "high level issue", Level: 3},
+		{Issue: "issue", Level: 2},
+	}
+
+	for _, request := range requests {
+		fmt.Println(junior.Handle(&request))
+	}
+}
